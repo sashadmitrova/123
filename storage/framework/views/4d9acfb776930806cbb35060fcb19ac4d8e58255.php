0103c021a7c57fb14ab16de782b5ad8b12c03a8a
Yra <zytiurfp@wsr.ru>
2023-02-16 19:41:57 -0800

    first commit

---
+++ storage/framework/views/4d9acfb776930806cbb35060fcb19ac4d8e58255.php
@@ -0,0 +1,95 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/resources/css/style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+
+
+
+
+
+      <div class="card-deck">
+        <div class="card">
+          <img class="card-img-top" src="/resources/img/21.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card </h5>
+            <p class="card-text">.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted"></small>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="/resources/img/31.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card </h5>
+            <p class="card-text">.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted"></small>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="/resources/img/41.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card </h5>
+            <p class="card-text"></p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted"></small>
+          </div>
+        </div>
+      </div>
+
+
+<div class="map">
+    <h1>Наше местоположение</h1>
+    <img src="/resources/img/map.jpg" alt="img" width="50%" height="50%">
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
+<?php /**PATH /srv/users/zytiurfp/kbvbhhv-m1/resources/views/katalog.blade.php ENDPATH**/ ?>
